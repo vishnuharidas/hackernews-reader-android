46c68fd69c93802a93c84983a1ee30567c0b9bbc
Vishnu Haridas <hvishnu999@gmail.com>
2022-04-12 00:25:33 +0530

    Initial commit

---
+++ app/src/main/java/com/qburst/hackernews/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.qburst.hackernews.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryOrange = Color(0xFFFF6600)
+val PrimaryOrangeVariant = Color(0xFFC64F00)
+val SecondaryOrange = Color(0xFFFF6600)
+val PrimaryTextColor = Color(0xFF000000)
