0c0e469e192452237beb062e006e7be653658c07
Vishnu Haridas <hvishnu999@gmail.com>
2022-04-13 03:26:52 +0530

    Add navigation to the MainActivity

--- app/src/main/java/com/qburst/hackernews/MainActivity.kt
+++ app/src/main/java/com/qburst/hackernews/MainActivity.kt
@@ -3,27 +3,23 @@ package com.qburst.hackernews
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.qburst.hackernews.ui.home.viewmodel.HomeUiState
-import com.qburst.hackernews.ui.home.viewmodel.HomeViewModel
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.qburst.hackernews.ui.home.HomeScreen
+import com.qburst.hackernews.ui.story_details.StoryDetails
 import com.qburst.hackernews.ui.theme.HackerNewsReaderTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    private val vm by viewModels<HomeViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,28 +31,27 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
 
-                    when (vm.uiState.state) {
+                    val navController = rememberNavController()
 
-                        HomeUiState.State.None,
-                        HomeUiState.State.Empty -> Text("Empty")
-                        HomeUiState.State.Error -> Text("Error: ${vm.uiState.error?.localizedMessage}")
-                        HomeUiState.State.Loading -> CircularProgressIndicator()
-                        HomeUiState.State.LoadingMore -> Text("Loading More")
-                        HomeUiState.State.Success -> LazyColumn() {
-                            vm.uiState.list.forEach {
-                                item(key = it.id) {
+                    NavHost(
+                        navController = navController,
+                        startDestination = "home"
+                    ) {
 
-                                    Text(
-                                        it.title ?: " !~~ ",
-                                        modifier = Modifier
-                                            .fillMaxWidth()
-                                            .clickable { }
-                                            .padding(16.dp)
-                                    )
-                                }
-                            }
+
+                        composable("home") {
+                            HomeScreen(navController)
                         }
 
+                        composable(
+                            "details/{storyId}",
+                            arguments = listOf(navArgument("storyId") { type = NavType.LongType })
+                        ) {
+                            StoryDetails(
+                                navController,
+                                it.arguments?.getLong("storyId") ?: 0L
+                            )
+                        }
 
                     }
 
@@ -65,6 +60,5 @@ class MainActivity : ComponentActivity() {
             }
         }
 
-        vm.getTopStories()
     }
 }
