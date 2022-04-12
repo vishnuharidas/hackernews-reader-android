d5467f83b21971e4a838945fa16040ac98ded320
Vishnu Haridas <hvishnu999@gmail.com>
2022-04-13 01:53:48 +0530

    TopStoriesRemoteSource now returns `Resource` instead of direct values

--- app/src/main/java/com/qburst/hackernews/data/repository/topstories/remote/TopStoriesRemoteSource.kt
+++ app/src/main/java/com/qburst/hackernews/data/repository/topstories/remote/TopStoriesRemoteSource.kt
@@ -2,10 +2,11 @@ package com.qburst.hackernews.data.repository.topstories.remote
 
 import com.qburst.hackernews.data.model.HNItem
 import com.qburst.hackernews.data.model.HNTopStories
+import com.qburst.hackernews.data.model.Resource
 
 interface TopStoriesRemoteSource {
 
-    suspend fun getTopStories(): HNTopStories?
+    suspend fun getTopStories(): Resource<HNTopStories>
 
-    suspend fun getStoryDetails(storyId: Long): HNItem?
+    suspend fun getStoryDetails(storyId: Long): Resource<HNItem>
 }
