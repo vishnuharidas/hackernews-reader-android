1a6fa26f5c2cd120f88efff0f247a612fc8c9a7d
Vishnu Haridas <hvishnu999@gmail.com>
2022-04-12 22:10:51 +0530

    Add HN API interface for Retrofit

---
+++ app/src/main/java/com/qburst/hackernews/data/repository/HackerNewsApi.kt
@@ -0,0 +1,20 @@
+package com.qburst.hackernews.data.repository
+
+import com.qburst.hackernews.data.model.HNItem
+import com.qburst.hackernews.data.model.HNTopStories
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+// Official HN API documentation: https://github.com/HackerNews/API
+interface HackerNewsApi {
+
+    @GET("topstories.json")
+    suspend fun getTopStories(): Response<HNTopStories>
+
+    @GET("item/{storyId}.json")
+    suspend fun getItemDetails(
+        @Query("storyId") storyId: Long
+    ): Response<HNItem>
+
+}
