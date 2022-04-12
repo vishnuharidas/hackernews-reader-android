a61a1e93f4860b976dc0309a1b0fbb4b3fdcad72
Vishnu Haridas <hvishnu999@gmail.com>
2022-04-13 01:54:16 +0530

    Update ApiSource to handle exceptions

--- app/src/main/java/com/qburst/hackernews/data/repository/topstories/remote/TopStoriesApiSource.kt
+++ app/src/main/java/com/qburst/hackernews/data/repository/topstories/remote/TopStoriesApiSource.kt
@@ -2,7 +2,9 @@ package com.qburst.hackernews.data.repository.topstories.remote
 
 import com.qburst.hackernews.data.model.HNItem
 import com.qburst.hackernews.data.model.HNTopStories
+import com.qburst.hackernews.data.model.Resource
 import com.qburst.hackernews.data.repository.HackerNewsApi
+import retrofit2.Response
 import javax.inject.Inject
 
 
@@ -11,11 +13,30 @@ class TopStoriesApiSource @Inject constructor(
 ) : TopStoriesRemoteSource {
 
 
-    override suspend fun getTopStories(): HNTopStories? =
-        hackerNewsApi.getTopStories().body()
+    override suspend fun getTopStories(): Resource<HNTopStories> = withExceptionHandling {
+        hackerNewsApi.getTopStories()
+    }
 
-    override suspend fun getStoryDetails(storyId: Long): HNItem? =
-        hackerNewsApi.getItemDetails(storyId = storyId).body()
 
+    override suspend fun getStoryDetails(storyId: Long): Resource<HNItem> = withExceptionHandling {
+        hackerNewsApi.getItemDetails(storyId = storyId)
+    }
+
+
+    private suspend fun <T : Any> withExceptionHandling(block: suspend () -> Response<T>): Resource<T> {
+
+        return try {
+            val response = block()
+
+            if (response.isSuccessful) {
+                Resource.Success(response.body() as T)
+            } else {
+                Resource.Error(Throwable(response.message()))
+            }
+
+        } catch (e: Exception) {
+            Resource.Error(Throwable(e.toString()))
+        }
+    }
 }
 
