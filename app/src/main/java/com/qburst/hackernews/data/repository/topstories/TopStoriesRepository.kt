e34c9f1bdb01bdf39a0623f7c8fb3973e625764a
Vishnu Haridas <hvishnu999@gmail.com>
2022-04-13 01:54:48 +0530

    Update 'Top Stories' repository to fetch top stories

--- app/src/main/java/com/qburst/hackernews/data/repository/topstories/TopStoriesRepository.kt
+++ app/src/main/java/com/qburst/hackernews/data/repository/topstories/TopStoriesRepository.kt
@@ -1,8 +1,13 @@
 package com.qburst.hackernews.data.repository.topstories
 
+import com.qburst.hackernews.data.model.HNItem
+import com.qburst.hackernews.data.model.Resource
 import com.qburst.hackernews.data.repository.topstories.remote.TopStoriesRemoteSource
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -10,16 +15,77 @@ class TopStoriesRepository @Inject constructor(
     private val remoteSource: TopStoriesRemoteSource
 ) {
 
+    private val _topMap = mutableMapOf<Long, HNItem?>()
+
+    private val _topStoriesFlow = MutableStateFlow<Resource<List<HNItem>>>(Resource.None)
+    val topStoriesFlow: Flow<Resource<List<HNItem>>> get() = _topStoriesFlow
+
     fun getTopStories() {
 
         CoroutineScope(Dispatchers.IO).launch {
 
-            val stories = remoteSource.getTopStories()
+            when (val resource = remoteSource.getTopStories()) {
 
-            stories?.forEach {
-                println("Story: $it")
+                Resource.None -> {}
+
+                is Resource.Error -> _topStoriesFlow.emit(resource)
+
+                is Resource.Success -> {
+
+                    // Add to the Map first, then fetch 10 items per page
+                    resource.data.forEach { _topMap[it] = null }
+
+                    nextPage()
+                }
             }
 
+        }
+
+    }
+
+    suspend fun nextPage() {
+
+        CoroutineScope(Dispatchers.IO).launch {
+
+            // Find the next 10 items with null values
+            coroutineScope {
+
+                _topMap.filter { it.value == null }.keys.take(10).forEach { storyId ->
+
+                    launch {
+                        val item = getStoryDetails(storyId)
+
+                        item?.let { hnItem ->
+                            _topMap[storyId] = hnItem
+
+                            // Emit the stories
+                            _topStoriesFlow.emit(
+                                Resource.Success(_topMap.values.toList().filterNotNull())
+                            )
+                        }
+                    }
+
+
+                }
+            }
+
+            // If no data are available even after trying to fetch items, send an error text
+            if (_topMap.values.filterNotNull().isEmpty()) {
+                _topStoriesFlow.emit(
+                    Resource.Success(emptyList())
+                )
+            }
+        }
+
+    }
+
+
+    private suspend fun getStoryDetails(storyId: Long): HNItem? {
+
+        return when (val res = remoteSource.getStoryDetails(storyId = storyId)) {
+            is Resource.Error -> null
+            Resource.None -> null
+            is Resource.Success -> res.data
         }
 
     }
