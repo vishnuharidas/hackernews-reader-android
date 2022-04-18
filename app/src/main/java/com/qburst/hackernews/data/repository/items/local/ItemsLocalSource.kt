fda26a4a865a0d72e92a77aef1483e74816dfba6
Vishnu Haridas <hvishnu999@gmail.com>
2022-04-18 23:33:53 +0530

    Items local source: add functions to cache the top stories, and a validation check

--- app/src/main/java/com/qburst/hackernews/data/repository/items/local/ItemsLocalSource.kt
+++ app/src/main/java/com/qburst/hackernews/data/repository/items/local/ItemsLocalSource.kt
@@ -1,8 +1,15 @@
 package com.qburst.hackernews.data.repository.items.local
 
 import com.qburst.hackernews.data.model.HNItem
+import com.qburst.hackernews.data.model.HNTopStories
 
 interface ItemsLocalSource {
+
+    suspend fun isValid(): Boolean
+
+    suspend fun saveTopStories(stories: HNTopStories)
+
+    suspend fun getTopStories(): HNTopStories
 
     suspend fun saveItem(item: HNItem)
 
