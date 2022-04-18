b89fe1d7126dbe5357ae2d5dcb2c8b58f58e97b9
Vishnu Haridas <hvishnu999@gmail.com>
2022-04-19 00:56:05 +0530

    Add UseCase to convert timestamp into "time ago" text

---
+++ app/src/main/java/com/qburst/hackernews/domain/GetTimeAgoUseCase.kt
@@ -0,0 +1,48 @@
+package com.qburst.hackernews.domain
+
+object GetTimeAgoUseCase {
+
+    private const val SECOND_MILLIS = 1000
+    private const val MINUTE_MILLIS = 60 * SECOND_MILLIS
+    private const val HOUR_MILLIS = 60 * MINUTE_MILLIS
+    private const val DAY_MILLIS = 24 * HOUR_MILLIS
+
+    operator fun invoke(t: Long?, default: String = "sometime"): String {
+
+        t ?: return default
+
+        val time = if (t < 1_000_000_000_000L) {
+            t * 1000
+        } else t
+
+        val now = System.currentTimeMillis()
+        if (time == 0L || time > now || time <= 0) {
+            return default
+        }
+
+        val diff = now - time
+        return when {
+            diff < MINUTE_MILLIS -> {
+                "just now"
+            }
+            diff < 2 * MINUTE_MILLIS -> {
+                "a minute ago"
+            }
+            diff < 50 * MINUTE_MILLIS -> {
+                "${diff / MINUTE_MILLIS} minutes"
+            }
+            diff < 90 * MINUTE_MILLIS -> {
+                "an hour ago"
+            }
+            diff < 24 * HOUR_MILLIS -> {
+                "${diff / HOUR_MILLIS} hours"
+            }
+            diff < 48 * HOUR_MILLIS -> {
+                "yesterday"
+            }
+            else -> {
+                "${diff / DAY_MILLIS} days"
+            }
+        }
+    }
+}
