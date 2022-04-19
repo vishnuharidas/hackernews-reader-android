90000c4581f85a99a2db5f26a9bc29c68dad345d
Vishnu Haridas <hvishnu999@gmail.com>
2022-04-20 01:03:06 +0530

    Update the item details screen to show the comments

--- app/src/main/java/com/qburst/hackernews/ui/story_details/ItemDetailsScreen.kt
+++ app/src/main/java/com/qburst/hackernews/ui/story_details/ItemDetailsScreen.kt
@@ -3,15 +3,17 @@ package com.qburst.hackernews.ui.story_details
 import android.content.Intent
 import android.net.Uri
 import android.text.Html
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowForward
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,7 +27,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import com.qburst.hackernews.data.model.HNItem
 import com.qburst.hackernews.data.model.HNItemType
 import com.qburst.hackernews.data.model.getTypeValue
 import com.qburst.hackernews.domain.GetTimeAgoUseCase
@@ -54,19 +55,25 @@ fun ItemDetailsScreen(
 
             is ItemDetailsUiState.State.Failure ->
                 Text(
-                    state.error,
+                    viewModel.uiState.error ?: "Unknown error. Please try later.",
                     modifier = Modifier
                         .align(Alignment.Center)
                         .padding(16.dp)
                 )
 
-            is ItemDetailsUiState.State.Success ->
+            ItemDetailsUiState.State.LoadingMore,
+            is ItemDetailsUiState.State.Success -> {
                 ItemDetails(
-                    navController,
-                    state.item,
-                    modifier = Modifier
-                        .fillMaxSize()
+                    navController = navController,
+                    uiState = viewModel.uiState,
+                    modifier = Modifier.fillMaxSize(),
+                    onNextPage = {
+                        viewModel.fetchNextComments()
+                    }
                 )
+
+            }
+
         }
 
     }
@@ -76,96 +83,238 @@ fun ItemDetailsScreen(
 @Composable
 private fun ItemDetails(
     navController: NavHostController,
-    item: HNItem,
-    modifier: Modifier = Modifier
+    uiState: ItemDetailsUiState,
+    modifier: Modifier = Modifier,
+    onNextPage: () -> Unit
 ) {
+
+    val item = uiState.item
+    val comments = uiState.comments
+
+    if (item == null) {
+        Box(
+            modifier = modifier
+        ) {
+            Text(
+                uiState.error ?: "U```q2nknown error. Please try later.",
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .padding(16.dp)
+            )
+        }
+        return
+    }
 
     val context = LocalContext.current
 
-    Column(
+    val listState = rememberLazyListState()
+
+    val canLoadMore = remember {
+
+        derivedStateOf {
+
+            val lastVisibleItem = listState.layoutInfo.visibleItemsInfo.lastOrNull() ?: return@derivedStateOf true
+
+            lastVisibleItem.index >= listState.layoutInfo.totalItemsCount - 2 // 2nd last item
+
+        }
+    }
+
+    LaunchedEffect(canLoadMore) {
+        snapshotFlow { canLoadMore.value }
+            .collect {
+                if (it) onNextPage()
+            }
+    }
+
+    Box(
         modifier = modifier
-            .verticalScroll(rememberScrollState())
-            .padding(16.dp)
     ) {
 
-        Row(
-            modifier = Modifier.fillMaxSize(),
-            verticalAlignment = Alignment.Top
+        LazyColumn(
+            modifier = modifier,
+            state = listState
         ) {
 
-            Text(
-                item.title ?: "-",
-                style = TextStyle(
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.Normal
-                ),
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(end = if (item.url != null) 8.dp else 0.dp)
-            )
-
-            if (item.url != null) {
-                IconButton(
-                    onClick = {
-
-                        if (item.getTypeValue() == HNItemType.Story
-                            && item.title?.startsWith("Ask HN:") == false /* Ask HN will be opened in a screen */) {
-                            context.startActivity(
-                                Intent(Intent.ACTION_VIEW, Uri.parse(item.url))
-                            )
-                        }
-
-                    },
+            item {
+                Row(
                     modifier = Modifier
-                        .border(width = 1.dp, color = Color.LightGray, shape = CircleShape)
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                        .padding(top = 16.dp),
+                    verticalAlignment = Alignment.Top
                 ) {
 
-                    Icon(Icons.Outlined.ArrowForward, contentDescription = "Open in Browser")
+                    Text(
+                        item.title ?: "-",
+                        style = TextStyle(
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Normal
+                        ),
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(end = if (item.url != null) 8.dp else 0.dp)
+                    )
+
+                    if (item.url != null) {
+                        IconButton(
+                            onClick = {
+
+                                if (item.getTypeValue() == HNItemType.Story
+                                    && item.title?.startsWith("Ask HN:") == false /* Ask HN will be opened in a screen */) {
+                                    context.startActivity(
+                                        Intent(Intent.ACTION_VIEW, Uri.parse(item.url))
+                                    )
+                                }
+
+                            },
+                            modifier = Modifier
+                                .border(width = 1.dp, color = Color.LightGray, shape = CircleShape)
+                        ) {
+
+                            Icon(Icons.Outlined.ArrowForward, contentDescription = "Open in Browser")
+
+                        }
+                    }
 
                 }
             }
 
-        }
+            item {
+                val meta = buildAnnotatedString {
 
-        val meta = buildAnnotatedString {
+                    append(item.score?.toString() ?: "0")
+                    append(" points")
+                    append(" by ")
+                    withStyle(SpanStyle(fontWeight = FontWeight.Medium)) {
+                        append(item.by ?: "Unknown")
+                    }
 
-            append(item.score?.toString() ?: "0")
-            append(" points")
-            append(" by ")
-            withStyle(SpanStyle(fontWeight = FontWeight.Medium)) {
-                append(item.by ?: "Unknown")
+                    append(" posted ")
+                    withStyle(SpanStyle(fontWeight = FontWeight.Medium)) {
+                        append(GetTimeAgoUseCase(item.time))
+                    }
+                    append(" ago")
+
+                    append("  \uD83D\uDCAC ")
+                    append(item.descendants?.toString() ?: "no comments")
+                }
+
+                Text(
+                    meta,
+                    style = TextStyle(
+                        color = Color.Gray
+                    ),
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
             }
 
-            append(" posted ")
-            withStyle(SpanStyle(fontWeight = FontWeight.Medium)) {
-                append(GetTimeAgoUseCase(item.time))
+            if (!item.text.isNullOrBlank()) { // "Ask HN" post will have text
+                item {
+                    Text(
+                        Html.fromHtml(item.text, Html.FROM_HTML_MODE_LEGACY).toString(),
+                        style = TextStyle(
+                            fontSize = 18.sp
+                        ),
+                        modifier = Modifier
+                            .padding(horizontal = 16.dp)
+                            .padding(top = 16.dp)
+                    )
+                }
             }
-            append(" ago")
 
-            append("  \uD83D\uDCAC ")
-            append(item.descendants?.toString() ?: "no comments")
+            item {
+                Divider(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .padding(vertical = 16.dp)
+                )
+            }
+
+            if (!comments.isNullOrEmpty()) {
+
+                comments.filter { it.deleted != true }
+                    .forEach { comment ->
+
+                        item {
+
+                            Column(
+                                modifier
+                                    .clickable { }
+                                    .padding(vertical = 16.dp)
+                            ) {
+
+                                val meta = buildAnnotatedString {
+
+                                    append("by ")
+                                    withStyle(SpanStyle(fontWeight = FontWeight.Medium)) {
+                                        append(comment.by ?: "Unknown")
+                                    }
+
+                                    append(" ")
+                                    withStyle(SpanStyle(fontWeight = FontWeight.Medium)) {
+                                        append(GetTimeAgoUseCase(comment.time))
+                                    }
+
+                                    if (comment.kids?.size ?: 0 > 0) {
+                                        append(" ")
+                                        append(comment.kids?.size?.toString() ?: "0")
+                                        append(" replies")
+                                    }
+
+                                }
+
+                                Text(
+                                    meta,
+                                    style = TextStyle(
+                                        color = Color.Gray
+                                    ),
+                                    modifier = Modifier
+                                        .padding(horizontal = 16.dp)
+                                )
+
+                                Text(
+                                    Html.fromHtml(comment.text ?: "---", Html.FROM_HTML_MODE_LEGACY).toString(),
+                                    style = TextStyle(
+                                        fontSize = 15.sp
+                                    ),
+                                    modifier = Modifier
+                                        .padding(horizontal = 16.dp)
+                                        .padding(top = 8.dp)
+                                )
+
+                            }
+
+                        }
+
+                        item {
+                            Divider()
+                        }
+
+                    }
+
+            }
+
         }
 
-        Text(
-            meta,
-            style = TextStyle(
-                color = Color.Gray
-            )
-        )
 
-        if (!item.text.isNullOrBlank()) { // "Ask HN" post will have text
-            Text(
-                Html.fromHtml(item.text, Html.FROM_HTML_MODE_LEGACY).toString(),
-                style = TextStyle(
-                    fontSize = 18.sp
-                ),
-                modifier = Modifier.padding(top = 16.dp)
-            )
+        // Loading more indicator
+        if (uiState.state == ItemDetailsUiState.State.LoadingMore) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .fillMaxWidth()
+                    .background(Color.White.copy(alpha = 0.5f))
+                    .padding(8.dp)
+            ) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .size(20.dp),
+                    strokeWidth = 2.dp
+                )
+            }
         }
-
-        Divider(
-            modifier = Modifier.padding(vertical = 16.dp)
-        )
 
     }
 }
